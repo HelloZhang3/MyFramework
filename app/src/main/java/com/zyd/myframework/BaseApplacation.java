8705ac049c7f5a9f5585ef35ef0233cdecd1ba28
zhangyadong <yadong9416@gmail.com>
2016-05-02 00:06:29 +0800

    Initial commit

---
+++ app/src/main/java/com/zyd/myframework/BaseApplacation.java
@@ -0,0 +1,20 @@
+package com.zyd.myframework;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/5/1 0001.
+ */
+public class BaseApplacation extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+}
