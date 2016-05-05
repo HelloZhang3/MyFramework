8f0d52a0f5c43466bfb6d747302c8791d284a30f
zhangyadong <yadong9416@gmail.com>
2016-05-05 22:57:22 +0800

    同步

--- app/src/main/java/com/zyd/myframework/MainActivity.java
+++ app/src/main/java/com/zyd/myframework/MainActivity.java
@@ -1,13 +1,28 @@
 package com.zyd.myframework;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.zyd.myframework.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int bindLayout() {
+        return 0;
     }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
 }
